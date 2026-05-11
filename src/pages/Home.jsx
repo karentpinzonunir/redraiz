cec78c06118033c3ea92d3d0c2accb2bb88e31ef
karentpinzonunir <karent.pinzon126@comunidadunir.net>
2026-05-11 18:37:52 -0500

    ajustes de estilos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import ButtonPrimary from '../components/ButtonPrimary';
+import ButtonSecondary from '../components/ButtonSecondary';
 
 const Home = () => {
   return (
     <div>
       <h1>Bienvenido a RedRaiz</h1>
+      <ButtonPrimary>
+        Boton Primario
+      </ButtonPrimary>
+      <ButtonSecondary>
+        Boton Secundario
+      </ButtonSecondary>
     </div>
+
+
   );
 };
 
